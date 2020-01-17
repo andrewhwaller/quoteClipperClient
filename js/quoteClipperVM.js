8b2aff3964199a980b930f4264a33e6c84a40745
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2020-01-16 22:12:15 -0600

    Update css/html, add PNG converter functions

--- js/quoteClipperVM.js
+++ js/quoteClipperVM.js
@@ -9,13 +9,12 @@ let homeVM = function () {
 
     self.userImage.subscribe(function () {
         if (self.userImage()) {
-            const selectedFile = document.getElementById('customFile').files[0];
-            self.processImage(selectedFile);
+            self.processImage(self.userImage());
         }
     })
 
     self.processImage = function (uploadedImage) {
-        self.handleFileFormat(uploadedImage)
+        // self.handleFileFormat(uploadedImage)
         self.spinUp();
         Tesseract.recognize(
             uploadedImage,
@@ -32,15 +31,15 @@ let homeVM = function () {
           })
     }
 
-    self.handleFileFormat = (uploadedImage) => {
-        let codename = uploadedImage.name;
-        console.log(uploadedImage.type)
-        if (uploadedImage.type === "image/heic") {
+    // self.handleFileFormat = (uploadedImage) => {
+    //     let codename = uploadedImage.name;
+    //     console.log(uploadedImage.type)
+    //     if (uploadedImage.type === "image/heic") {
             
-        } else {
-            return
-        }
-    }
+    //     } else {
+    //         return
+    //     }
+    // }
 
     self.spinUp = () => {
         self.text("");
@@ -73,6 +72,31 @@ let homeVM = function () {
 }
 
 let vm = new homeVM();
+
+let fileInput = document.getElementById('customFile');
+
+fileInput.addEventListener('change', function(event) {
+    if(event.target.files) {
+        let file = event.target.files[0];
+        var reader  = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function (event) {
+            return new Promise((res, rej)=>{
+                var image = new Image();
+                image.src = event.target.result;
+                res(image);
+            }).then((image)=>{
+                var canvas = document.getElementById('canvas');
+                canvas.width = image.width;
+                canvas.height = image.height;
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(image,0,0);
+                var fullQuality = canvas.toDataURL('image/png');
+                vm.userImage(fullQuality);
+            })
+        }
+      }
+   });
 
 let loadDependencies = async () => {
     await bind();
