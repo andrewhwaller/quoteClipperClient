55c3431f9644d9e72319a0806a4ed488798ec2e2
andrewhwaller <andrewhwaller@gmail.com>
2020-02-02 17:36:20 -0600

    Add login redirect logic

--- js/login.js
+++ js/login.js
@@ -1,27 +1,37 @@
-const loginForm = document.getElementById('loginForm')
-
-if (Cookies.get('auth_token')) {
-    window.location = "/index.html"
-}
-
 loginForm.addEventListener('submit', function (e) {
     e.preventDefault();
+    let status;
     let headers = new Headers();
     headers.set('Content-type', 'application/json');
-
     fetch("https://afternoon-fjord-40383.herokuapp.com/api/v1/users/login", {
         method: 'POST',
         body: JSON.stringify({ email: loginForm.email.value, password: loginForm.password.value }),
-        // credentials: 'include',
         headers: headers
     }).then((response) => {
+        status = response.status;
         return response.json()
     }).then((json) => {
-        console.log(json.auth_token)
         Cookies.set('auth_token', json.auth_token, { expires: 2 });
-        // Cookies.set(auth_token, { expires: 7 })
+        if (status === 200) {
+            window.location = "/index.html"
+        }
     }).catch(function (error) {
         console.log(error);
         alert("Error! Please try again.");
     });
 });
+
+function init() {
+    const loginForm = document.getElementById('loginForm')
+    const loginPanel = document.getElementById("loginPanel")
+    const spinner = document.getElementById("spinner")
+    
+    if (Cookies.get('auth_token')) {
+        window.location = "/index.html"
+    } else {
+        spinner.style.display = "none"
+        loginPanel.style.display = "flex"
+    }
+}
+
+init();
