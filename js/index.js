2a81ab9914f6b5e7062d4608a7f6d3ac9e30b1e9
andrewhwaller <andrewhwaller@gmail.com>
2020-02-02 18:06:36 -0600

    Change location for js files

---
+++ js/index.js
@@ -0,0 +1,37 @@
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let status;
+    let headers = new Headers();
+    headers.set('Content-type', 'application/json');
+    fetch("https://afternoon-fjord-40383.herokuapp.com/api/v1/users/login", {
+        method: 'POST',
+        body: JSON.stringify({ email: loginForm.email.value, password: loginForm.password.value }),
+        headers: headers
+    }).then((response) => {
+        status = response.status;
+        return response.json()
+    }).then((json) => {
+        Cookies.set('auth_token', json.auth_token, { expires: 2 });
+        if (status === 200) {
+            window.location = "/quoteClipper.html"
+        }
+    }).catch(function (error) {
+        console.log(error);
+        alert("Invalid credentials! Please try again.");
+    });
+});
+
+function init() {
+    const loginForm = document.getElementById('loginForm')
+    const loginPanel = document.getElementById("loginPanel")
+    const spinner = document.getElementById("spinner")
+    
+    if (Cookies.get('auth_token')) {
+        window.location = "/quoteClipper.html"
+    } else {
+        spinner.style.display = "none"
+        loginPanel.style.display = "flex"
+    }
+}
+
+init();
